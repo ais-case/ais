e5cc2ea6341be2e4edad42927dac09986872a107
John <j.a.hornlopes@student.tudelft.nl>
2012-05-09 18:53:55 +0200

    Better unit tests of Service, fixed problem with message sending

--- lib/service.rb
+++ lib/service.rb
@@ -7,24 +7,26 @@ class TransmitterProxy
     
     def send_position_report_for(vessel)
         message = Marshal.dump(vessel)
-        @socket.send(message)
+        @socket.send_string(message)
     end
 end
 
 class Service
     ENDPOINTS = { :transmitter => 'tcp://localhost:20010'}
 
-    def initialize()
-        @context = ZMQ::Context.new
+    def initialize(context=ZMQ::Context.new)
+        @context = context
     end
-
+    
     def bind(name)
         ep = ENDPOINTS[:transmitter]
         socket = @context.socket ZMQ::REQ
-        rc = socket.connect ep 
+        rc = socket.connect ep
+
         if ZMQ::Util.resultcode_ok? rc
+            return TransmitterProxy.new socket    
+        else
             raise RuntimeError, "Couldn't connect to #{ep}"
         end
-        return TransmitterProxy.new socket
     end
 end
--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -4,9 +4,34 @@ require "service"
 describe Service do
     describe "bind" do
         it "returns a service binding" do
-            service = Service.new
+            socket = double("Socket")
+            socket.stub(:connect) { 0 }
+            
+            context = double("Context")
+            context.stub(:socket) { socket }
+            
+            service = Service.new context
+
             ret = service.bind 'ais/transmitter'
             ret.should be_a_kind_of(TransmitterProxy)
+            
+            socket.stub(:connect) { -1 }
+            service = Service.new context
+            expect { service.bind 'ais/transmitter' }.to raise_error
+        end
+
+        it "raises an exception when the socket fails" do
+            socket = double("Socket")
+            socket.stub(:connect) { -1 }
+            
+            context = double("Context")
+            context.stub(:socket) { socket }
+            
+            service = Service.new context
+
+            socket.stub(:connect) { -1 }
+            service = Service.new context
+            expect { service.bind 'ais/transmitter' }.to raise_error
         end
     end    
 end
@@ -15,7 +40,7 @@ describe "TransmitterProxy" do
     it "sends position reports to the Transmitter service" do
         vessel = "Vessel"
         socket = double('Socket')
-        socket.should_receive(:send).with(Marshal.dump(vessel))
+        socket.should_receive(:send_string).with(Marshal.dump(vessel))
 
         t = TransmitterProxy.new socket
         t.send_position_report_for vessel
