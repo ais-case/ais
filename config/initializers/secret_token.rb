db87900b8dfe798f517303c3655b3cf039c2b659
John <j.a.hornlopes@student.tudelft.nl>
2012-05-10 10:24:00 +0200

    Added rails, moved all code into rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ais::Application.config.secret_token = 'ab61610baf4655b0f508cefa4b4c4b150000a3e262e5b70ff10c2fec2e614306df010182b2e30cd278127fb28f59a0f631e58d7e2372b9ff75196dd3af850049'
