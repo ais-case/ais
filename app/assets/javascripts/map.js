794de4b058ab13c25c360101c340bd7e6309e55d
John <j.a.hornlopes@student.tudelft.nl>
2012-05-16 23:08:47 +0200

    Fixed style in accordance with Closure Linter

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,25 +1,26 @@
-"use strict";
+'use strict';
 var map = null;
 
 var parseLocations = function(s) {
-	var pair = s.split('_').map(function(loc) {
-		var coords = loc.split(',').map(parseFloat);
-		if (coords.length != 2) throw "Invalid coordinates";
-		return new LatLon(coords[0], coords[1]);
-	});
-	if (pair.length != 2) throw "Invalid location";
-	return pair;
-}
+  var pair = s.split('_').map(function(loc) {
+    var coords = loc.split(',').map(parseFloat);
+    if (coords.length != 2) throw 'Invalid coordinates';
+    return new LatLon(coords[0], coords[1]);
+  });
+  if (pair.length != 2) throw 'Invalid location';
+  return pair;
+};
 
 $(document).ready(function() {
-	map = new Map('map', new LatLon(52.1,3.9))
-	var locations;
-	try {
-		locations = parseLocations(window.location.hash.substring(1));
-	} catch (err) {
-		locations = null;
-	}
-	if (locations != null) {
-		map.zoomToArea(locations[0], locations[1]);
-	}
+  map = new Map('map', new LatLon(52.1, 3.9));
+  map.loadMarkers(new AjaxDataLoader('/map/markers'));
+  var locations;
+  try {
+    locations = parseLocations(window.location.hash.substring(1));
+  } catch (err) {
+    locations = null;
+  }
+  if (locations != null) {
+    map.zoomToArea(locations[0], locations[1]);
+  }
 });
