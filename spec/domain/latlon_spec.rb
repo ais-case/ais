87338fb442ebf6aa0a5d76e9be36639d0b7bea64
John <j.a.hornlopes@student.tudelft.nl>
2012-05-18 20:18:20 +0200

    Added explicit module names to specs

--- spec/domain/latlon_spec.rb
+++ spec/domain/latlon_spec.rb
@@ -1,44 +1,46 @@
 require 'spec_helper'
 
-describe Domain::LatLon do
-    it "can create a new LatLon from a string" do
-      latlon = LatLon.from_str("47.16N, 9.66E")
-      latlon.lat.should eq 47.16
-      latlon.lon.should eq 9.66
-
-      latlon = LatLon.from_str("47.16N, 9.66W")
-      latlon.lat.should eq 47.16
-      latlon.lon.should eq -9.66
-
-      latlon = LatLon.from_str("47.16S, 9.66W")
-      latlon.lat.should eq -47.16
-      latlon.lon.should eq -9.66
-
-      latlon = LatLon.from_str("47.16S, 9.66E")
-      latlon.lat.should eq -47.16
-      latlon.lon.should eq 9.66
-
-      latlon = LatLon.from_str("47N, 9E")
-      latlon.lat.should eq 47.0
-      latlon.lon.should eq 9.0
-    end
-    
-    it "rejects invalid string input" do
-      expect { LatLon.from_str " 1.1N, 9.6E" }.to raise_error
-      expect { LatLon.from_str "1.1 N, 9.6E" }.to raise_error
-      expect { LatLon.from_str "1 1.1N, 9.6E" }.to raise_error
-    end
-    
-    it 'can be converted to a string' do
-      latlon = LatLon.from_str("47.16N, 9.66W")
-      latlon.to_s.should eq "47.16,-9.66"
-    end
-    
-    it "can be compared to other LatLon objects" do
-      l = LatLon.new(1.0, 2.5)
+module Domain
+  describe LatLon do
+      it "can create a new LatLon from a string" do
+        latlon = LatLon.from_str("47.16N, 9.66E")
+        latlon.lat.should eq 47.16
+        latlon.lon.should eq 9.66
+  
+        latlon = LatLon.from_str("47.16N, 9.66W")
+        latlon.lat.should eq 47.16
+        latlon.lon.should eq -9.66
+  
+        latlon = LatLon.from_str("47.16S, 9.66W")
+        latlon.lat.should eq -47.16
+        latlon.lon.should eq -9.66
+  
+        latlon = LatLon.from_str("47.16S, 9.66E")
+        latlon.lat.should eq -47.16
+        latlon.lon.should eq 9.66
+  
+        latlon = LatLon.from_str("47N, 9E")
+        latlon.lat.should eq 47.0
+        latlon.lon.should eq 9.0
+      end
       
-      l.should eq(LatLon.new(1.0, 2.5))
-      l.should_not eq(LatLon.new(2.0, 2.5))
-      l.should_not eq(LatLon.new(1.0, 2.0))
-    end
+      it "rejects invalid string input" do
+        expect { LatLon.from_str " 1.1N, 9.6E" }.to raise_error
+        expect { LatLon.from_str "1.1 N, 9.6E" }.to raise_error
+        expect { LatLon.from_str "1 1.1N, 9.6E" }.to raise_error
+      end
+      
+      it 'can be converted to a string' do
+        latlon = LatLon.from_str("47.16N, 9.66W")
+        latlon.to_s.should eq "47.16,-9.66"
+      end
+      
+      it "can be compared to other LatLon objects" do
+        l = LatLon.new(1.0, 2.5)
+        
+        l.should eq(LatLon.new(1.0, 2.5))
+        l.should_not eq(LatLon.new(2.0, 2.5))
+        l.should_not eq(LatLon.new(1.0, 2.0))
+      end
+  end
 end
--- spec/domain/vessel_spec.rb
+++ spec/domain/vessel_spec.rb
@@ -1,33 +1,35 @@
 require 'spec_helper'
 
-describe Domain::Vessel do
-  it "requires a mmsi and class" do
-    vessel = Vessel.new(1234, Vessel::CLASS_A)
-    vessel.mmsi.should eq 1234
-    vessel.vessel_class.should eq Vessel::CLASS_A
-  end
-  
-  it "has name and position attributes" do
-    vessel = Vessel.new(1234, Vessel::CLASS_A)
-    vessel.name = "Seal"
-    vessel.position = LatLon.new(50.0, 4.0)
-
-    vessel.name.should eq "Seal"
-    vessel.position.lat.should eq 50.0
-    vessel.position.lon.should eq 4.0
-  end
-  
-  it "can be compared to other vessels" do
-    v1 = Vessel.new(1234, Vessel::CLASS_A)
-    v1.position = LatLon.new(10.0, 4.0)
-    v2 = Vessel.new(1234, Vessel::CLASS_A)
-    v2.position = LatLon.new(10.0, 4.0)
-    v3 = Vessel.new(5678, Vessel::CLASS_A)
-    v3.position = LatLon.new(10.0, 4.0)
-    v1.should eq(v2)
-    v2.should_not eq(v3)
+module Domain
+  describe Vessel do
+    it "requires a mmsi and class" do
+      vessel = Vessel.new(1234, Vessel::CLASS_A)
+      vessel.mmsi.should eq 1234
+      vessel.vessel_class.should eq Vessel::CLASS_A
+    end
     
-    v2.position = LatLon.new(0.0, 0.0)
-    v1.should_not eq(v2)
+    it "has name and position attributes" do
+      vessel = Vessel.new(1234, Vessel::CLASS_A)
+      vessel.name = "Seal"
+      vessel.position = LatLon.new(50.0, 4.0)
+  
+      vessel.name.should eq "Seal"
+      vessel.position.lat.should eq 50.0
+      vessel.position.lon.should eq 4.0
+    end
+    
+    it "can be compared to other vessels" do
+      v1 = Vessel.new(1234, Vessel::CLASS_A)
+      v1.position = LatLon.new(10.0, 4.0)
+      v2 = Vessel.new(1234, Vessel::CLASS_A)
+      v2.position = LatLon.new(10.0, 4.0)
+      v3 = Vessel.new(5678, Vessel::CLASS_A)
+      v3.position = LatLon.new(10.0, 4.0)
+      v1.should eq(v2)
+      v2.should_not eq(v3)
+      
+      v2.position = LatLon.new(0.0, 0.0)
+      v1.should_not eq(v2)
+    end
   end
 end
