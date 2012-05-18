3bbe973bfdfbdbd341f8fc2e77b7aaa3d8ce05e2
John <j.a.hornlopes@student.tudelft.nl>
2012-05-18 16:15:40 +0200

    Use different ports for different tests

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -51,11 +51,11 @@ describe Service::BaseService do
       end
     end
     service = EchoServiceMock.new
-    service.start 'tcp://*:21000'
+    service.start 'tcp://*:22000'
      
     ctx = ZMQ::Context.new
     sock = ctx.socket ZMQ::REQ
-    rc = sock.connect 'tcp://localhost:21000'
+    rc = sock.connect 'tcp://localhost:22000'
     ZMQ::Util.resultcode_ok?(rc).should be_true
     begin
       sock.send_string(Marshal.dump("Test"))
