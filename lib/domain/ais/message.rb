0989121802358f335cba3f45004c797e09f48a7e
John <j.a.hornlopes@student.tudelft.nl>
2012-05-21 09:52:54 +0200

    Added some properties to Message class

--- lib/domain/ais/message.rb
+++ lib/domain/ais/message.rb
@@ -1,9 +1,11 @@
 module Domain::AIS
   class Message
-    attr_reader :mmsi
+    attr_reader :mmsi, :vessel_class
+    attr_accessor :lat, :lon
     
     def initialize(mmsi)
       @mmsi = mmsi
+      @vessel_class = Domain::Vessel::CLASS_A
     end
   end
 end
--- spec/domain/ais/message_spec.rb
+++ spec/domain/ais/message_spec.rb
@@ -2,9 +2,18 @@ require 'spec_helper'
 
 module Domain::AIS
   describe Message do
-    it "has a mmsi property" do
+    it "has mmsi, vessel_class properties" do
       m = Message.new(244314000)
       m.mmsi.should eq(244314000)
+      m.vessel_class.should eq(Domain::Vessel::CLASS_A)
+    end
+    
+    it "has lat and lon properties" do  
+      m = Message.new(244314000)
+      m.lat = 1.0
+      m.lat.should eq(1.0)
+      m.lon = -1.0
+      m.lon.should eq(-1.0)
     end    
   end
 end
