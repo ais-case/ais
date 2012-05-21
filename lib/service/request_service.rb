841227d0157532a1786a2714839179ba86154138
John <j.a.hornlopes@student.tudelft.nl>
2012-05-21 13:54:19 +0200

    RequestService class can  be used to create a request service

---
+++ lib/service/request_service.rb
@@ -0,0 +1,40 @@
+require 'ffi-rzmq'
+
+module Service
+  class RequestService
+    def initialize(handler)
+      @handler = handler
+      @thread = nil
+    end
+    
+    def start(endpoint)
+      @thread = Thread.new do
+        context = ZMQ::Context.new
+        socket = context.socket(ZMQ::REP)        
+        begin            
+          socket.bind(endpoint)
+          loop do 
+            data = ''
+            socket.recv_string(data)
+            socket.send_string(@handler.call(data))
+          end
+        rescue
+          puts $!
+          raise
+        ensure 
+          socket.close
+        end
+      end
+  
+      sleep(2)
+      if not @thread.alive? 
+        raise RuntimeError, "Couldn't start service listener"
+      end 
+    end
+    
+    def stop
+      @thread.kill if @thread
+      @thread = nil
+    end
+  end  
+end
---
+++ spec/services/request_service_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+module Service
+  describe RequestService do
+    it "accepts requests on a socket" do
+      handler_class = Class.new do
+        def handle_request(data)
+          data
+        end
+      end
+
+      handler = handler_class.new
+      service = RequestService.new(handler.method(:handle_request))
+      service.start('tcp://*:22000')
+       
+      ctx = ZMQ::Context.new
+      sock = ctx.socket ZMQ::REQ
+      rc = sock.connect 'tcp://localhost:22000'
+      ZMQ::Util.resultcode_ok?(rc).should be_true
+      begin
+        sock.send_string(Marshal.dump("Test"))
+        response = ''
+        sock.recv_string(response)
+        response.should eq(Marshal.dump("Test"))
+      ensure
+        sock.close
+        service.stop
+      end
+    end    
+  end
+end
