920bc69d2157f1486c19ac7cdc50e9f67e3e033c
John <j.a.hornlopes@student.tudelft.nl>
2012-05-21 10:28:09 +0200

    Expanded vessel service to process a message into a vessel and add it to the internal list of vessels

--- lib/service/vessel_service.rb
+++ lib/service/vessel_service.rb
@@ -4,8 +4,15 @@ module Service
       @vessels = []
       @vessels_mutex = Mutex.new
     end
-        
-    def receiveVessel(vessel)
+
+    def processMessage(payload)
+      message = Domain::AIS::MessageFactory.fromPayload(payload)
+      vessel = Domain::Vessel.new(message.mmsi, message.vessel_class)
+      vessel.position = Domain::LatLon.new(message.lat, message.lon)
+      @vessels << vessel
+    end
+    
+     def receiveVessel(vessel)
       @vessels_mutex.synchronize do
         @vessels << vessel
       end
--- spec/services/vessel_service_spec.rb
+++ spec/services/vessel_service_spec.rb
@@ -4,6 +4,15 @@ module Service
   describe VesselService do
     it_behaves_like "a service"
     
+    it "can process AIS messages" do
+      payload = "13`wgT0P5fPGmDfN>o?TN?vN2<05"
+      service = VesselService.new
+      service.processMessage(payload)
+      vessel = Marshal.load(service.processRequest(''))[0]
+      vessel.vessel_class.should eq(Domain::Vessel::CLASS_A)
+      vessel.mmsi.should eq(244314000)
+    end
+    
     it "returns a list of vessels" do
       vessel1 = Domain::Vessel.new(1234, Domain::Vessel::CLASS_A)
       vessel1.position = Domain::LatLon.new(3.0, 4.0) 
