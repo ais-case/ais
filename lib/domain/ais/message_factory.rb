121a5c337f49c141e842aed4fd1243bd2afe828c
John <j.a.hornlopes@student.tudelft.nl>
2012-05-21 09:44:32 +0200

    Refactor: change from instance method to class method

--- lib/domain/ais/message_factory.rb
+++ lib/domain/ais/message_factory.rb
@@ -1,6 +1,6 @@
 module Domain::AIS
   class MessageFactory
-    def fromPayload(payload)
+    def self.fromPayload(payload)
       decoded = SixBitEncoding.decode(payload)
       msg_type = decoded[0..5].to_i(2)
       raise "Unknown message type #{msg_type}" unless [1, 2, 3].include?(msg_type)   
--- spec/domain/ais/message_factory_spec.rb
+++ spec/domain/ais/message_factory_spec.rb
@@ -5,7 +5,7 @@ module Domain::AIS
     it "can create a message object from a payload" do
       payload = "13`wgT0P5fPGmDfN>o?TN?vN2<05"
       mf = MessageFactory.new
-      msg = mf.fromPayload(payload)
+      msg = MessageFactory.fromPayload(payload)
       msg.mmsi.should eq(244314000)
     end    
   end
