e4cf32a55a583bd713b59f80d95d277f3382330c
John <j.a.hornlopes@student.tudelft.nl>
2012-05-22 08:41:50 +0200

    Transmitter broadcasts received reports on 'plain' tcp socket

--- lib/service/transmitter_service.rb
+++ lib/service/transmitter_service.rb
@@ -1,21 +1,59 @@
 module Service
   class TransmitterService < BaseService
     def initialize(registry)
-
       @reply_service = ReplyService.new(method(:process_request))
+      @messages = Queue.new
     end
     
     def start(endpoint)
       @reply_service.start(endpoint)
+      
+      @transmitter = Thread.new do
+        socket = TCPServer.new(20000)
+        begin
+          client = socket.accept
+          loop do
+            client.puts(@messages.pop)
+          end
+        ensure 
+          socket.close
+        end
+      end
+      
+      sleep(2)
     end
     
     def stop
       @reply_service.stop
+      @transmitter.kill if @transmitter
+    end
+        
+    def checksum(msg)
+      sum = 0 
+      msg.each_byte do |c|
+        sum^=c
+      end
+      return sum
     end
     
     def process_request(data)
-      # TODO compile information in AIS message and deliver it
-      # Nothing needs to be returned, empty response
+      vessel = Marshal.load(data)
+      int_class = Domain::AIS::Datatypes::Int 
+      payload = ''
+      payload << int_class.new(1).bit_string(6)
+      payload << '00'
+      payload << int_class.new(vessel.mmsi).bit_string(30)
+      payload << '0' * 23
+      payload << int_class.new(vessel.position.lon * 600_000).bit_string(28)
+      payload << int_class.new(vessel.position.lat * 600_000).bit_string(27)
+      payload << '0' * 51
+      
+      message = "AIVDM,1,1,,A,#{Domain::AIS::SixBitEncoding.encode(payload)},0*"
+      message << checksum(message).to_s(16)
+      message = "!" << message
+      
+      @messages.push(message)
+      
       ""
     end  
   end
--- spec/services/transmitter_service_spec.rb
+++ spec/services/transmitter_service_spec.rb
@@ -12,5 +12,28 @@ module Service
       service = TransmitterService.new(ServiceRegistry.new)
       service.process_request(Marshal.dump(vessel))
     end
+    
+    it "sends out updates" do
+      service = TransmitterService.new(ServiceRegistry.new)
+      service.start('tcp://*:27000')
+      socket = TCPSocket.new('localhost', 20000)      
+      begin          
+        vessel = Domain::Vessel.new(12345, Domain::Vessel::CLASS_A)
+        vessel.position = Domain::LatLon.new(3.0, 4.0)
+        service.process_request(Marshal.dump(vessel))
+        
+        data = "!AIVDM,1,1,,A,1000h>@00001m<00:w800000000,0*e"
+        preamble, fragment_count, fragment, id, channel, payload, checksum = data.split(',')      
+        type = Domain::AIS::SixBitEncoding.decode(payload[0]).to_i(2)
+        message = Domain::AIS::MessageFactory.fromPayload(payload)
+
+        timeout(1) do
+          socket.gets.should eq("!AIVDM,1,1,,A,1000h>@0000BCp01eo@00000000,0*21\n")
+        end
+      ensure
+        service.stop
+        socket.close
+      end
+    end
   end  
 end
