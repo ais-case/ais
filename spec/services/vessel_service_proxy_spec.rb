4bef8d52b4d23d8469c382429f0ef5f6b5f26b62
John <j.a.hornlopes@student.tudelft.nl>
2012-05-22 17:41:39 +0200

    Clean up vessel proxy spec

--- spec/services/vessel_service_proxy_spec.rb
+++ spec/services/vessel_service_proxy_spec.rb
@@ -3,25 +3,26 @@ require 'spec_helper'
 module Service
   describe VesselServiceProxy do
     it "requests vessel information from the Vessel service" do
+      
+      # Some test data
       vessel1 = Domain::Vessel.new(1234, Domain::Vessel::CLASS_A)
       vessel1.position = Domain::LatLon.new(3.0, 4.0) 
       vessel2 = Domain::Vessel.new(5678, Domain::Vessel::CLASS_A)
       vessel2.position = Domain::LatLon.new(5.0, 6.0)
       vessels = [vessel1, vessel2] 
+
+      # Mock socket      
+      socket = double('Socket')
+      socket.stub(:send_string)
+      socket.stub(:recv_string) do |str|
+        str.replace(Marshal.dump(vessels))
+      end
       
-      socket = (Class.new do
-        def initialize(vessels)
-          @vessels = vessels
-        end
-        
-        def send_string(string)
-        end
-        
-        def recv_string(string)
-          string.replace(Marshal.dump(@vessels))
-        end
-      end).new(vessels)
+      # Socket should be used correctly
+      socket.should_receive(:send_string).with('')
+      socket.should_receive(:recv_string)
       
+      # Mock should return correct list of vessels
       t = VesselServiceProxy.new(socket)
       t.vessels.should eq(vessels)
     end  
