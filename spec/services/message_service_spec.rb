5865f031b22428e1d2a46ae8e95761759fa90dd2
John <j.a.hornlopes@student.tudelft.nl>
2012-05-22 18:16:06 +0200

    Extracted the hardcoded constants into a few test variables

--- spec/services/message_service_spec.rb
+++ spec/services/message_service_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 module Service
   describe MessageService do
+    before(:all) do
+      @sample_message = "!AIVDM,1,1,,B,13OF<80vh2wgiJJNes7EMGrD0<0e,0*00"
+      @sample_type    =                1
+      @sample_payload =               "13OF<80vh2wgiJJNes7EMGrD0<0e"
+    end
+    
     it_behaves_like "a service"
     
     it "listens for raw AIS data from a local TCP server on port 20000" do
@@ -11,14 +17,14 @@ module Service
       server = Thread.new(TCPServer.new(20000)) do |socket|
         begin
           client = socket.accept
-          client.puts("!AIVDM,1,1,,B,13OF<80vh2wgiJJNes7EMGrD0<0e,0*00")
+          client.puts(@sample_message)
         ensure 
           socket.close
         end
       end
       
       service = MessageService.new(Platform::ServiceRegistry.new)
-      service.should_receive(:process_message).with("!AIVDM,1,1,,B,13OF<80vh2wgiJJNes7EMGrD0<0e,0*00\n")        
+      service.should_receive(:process_message).with(@sample_message << "\n")        
       service.start('tcp://*:28000')
       
       # Wait for mock TCP server to finish request
@@ -31,13 +37,13 @@ module Service
   
     it "publishes processed messages" do
       service = MessageService.new(Platform::ServiceRegistry.new)
-      service.should_receive(:publish_message).with(1,"13OF<80vh2wgiJJNes7EMGrD0<0e")
-      service.process_message("!AIVDM,1,1,,B,13OF<80vh2wgiJJNes7EMGrD0<0e,0*00")
+      service.should_receive(:publish_message).with(@sample_type, @sample_payload)
+      service.process_message(@sample_message)
     end
     
     it "broadcasts published messages to subscribers" do
       handler = double('Subscriber')
-      handler.should_receive(:handle_request).with("1 13OF<80vh2wgiJJNes7EMGrD0<0e")
+      handler.should_receive(:handle_request).with("#{@sample_type} #{@sample_payload}")
 
       subscr = Platform::SubscriberService.new(handler.method(:handle_request), ['1 '])
       
@@ -46,7 +52,7 @@ module Service
         service.start('tcp://*:29000')
 
         subscr.start('tcp://localhost:29000')    
-        service.publish_message(1,"13OF<80vh2wgiJJNes7EMGrD0<0e")
+        service.publish_message(@sample_type, @sample_payload)
         
         # Wait a very short time to allow for message delivery 
         sleep(0.05)
