0578e0fc11e304bcc3927c05edff4b0f44bc6935
John <j.a.hornlopes@student.tudelft.nl>
2012-05-22 18:27:32 +0200

    Use rspec double for mock

--- spec/services/platform/subscriber_service_spec.rb
+++ spec/services/platform/subscriber_service_spec.rb
@@ -3,32 +3,20 @@ require 'spec_helper'
 module Service::Platform
   describe SubscriberService do
     it "accepts lists for updates on a socket" do
-      handler_class = Class.new do
-        attr_reader :data
-        
-        def initialize
-          @data = nil
-        end
-        
-        def handle_request(data)
-          @data = data
-        end
-      end
-
-      handler = handler_class.new
+      handler = double('Handler')
+      handler.should_receive(:handle_request).with("TEST Hello World!")
       service = SubscriberService.new(handler.method(:handle_request), ["TEST"])
        
       ctx = ZMQ::Context.new
       sock = ctx.socket ZMQ::PUB
       rc = sock.bind('tcp://*:26000')
       ZMQ::Util.resultcode_ok?(rc).should be_true
-      sleep(2)
       begin
         service.start('tcp://localhost:26000')
+        sock.send_string("OTHER Other message 1!")
         sock.send_string("TEST Hello World!")
-        sock.send_string("OTHER Other message!")
-        sleep(1)
-        handler.data.should eq("TEST Hello World!")    
+        sock.send_string("OTHER Other message 2!")
+        sleep(0.05)
       ensure
         sock.close
         service.stop
