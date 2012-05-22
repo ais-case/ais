bd1cf1ddeae77e65738c736e2e3bb64fb041b955
John <j.a.hornlopes@student.tudelft.nl>
2012-05-22 08:40:59 +0200

    Use AIS datatypes when decoding values

--- lib/domain/ais/message_factory.rb
+++ lib/domain/ais/message_factory.rb
@@ -4,7 +4,7 @@ module Domain::AIS
       decoded = SixBitEncoding.decode(payload)
       msg_type = decoded[0..5].to_i(2)
       raise "Unknown message type #{msg_type}" unless [1, 2, 3].include?(msg_type)   
-      message = Message.new(decoded[8..37].to_i(2))
+      message = Message.new(Datatypes::Int.from_bit_string(decoded[8..37]).value)
       lon = Datatypes::Int.from_bit_string(decoded[61..88])
       lat = Datatypes::Int.from_bit_string(decoded[89..115])
       message.lon = lon.value / 600000.0
