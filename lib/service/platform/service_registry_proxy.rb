674835280c0bb6bdf63604e8b7c695691e970475
John <j.a.hornlopes@student.tudelft.nl>
2012-05-23 14:10:19 +0200

    Cleaned up spec for service registry proxy, added functionality

--- lib/service/platform/service_registry_proxy.rb
+++ lib/service/platform/service_registry_proxy.rb
@@ -6,18 +6,43 @@ require_relative '../transmitter_proxy'
 module Service
   module Platform
     class ServiceRegistryProxy
-      ENDPOINTS = { 
-        'ais/transmitter' => { :endpoint => 'tcp://localhost:21000', :class => Service::TransmitterProxy},
-        'ais/vessels'     => { :endpoint => 'tcp://localhost:21001', :class => Service::VesselServiceProxy},
-        'ais/message'     => { :endpoint => 'tcp://localhost:21002', :class => nil}
+      attr_writer :context
+      
+      PROXIES = { 
+        'ais/transmitter' => Service::TransmitterProxy,
+        'ais/vessels'     => Service::VesselServiceProxy,
+        'ais/message'     => nil
       }
     
-      def initialize(context=ZMQ::Context.new)
-        @context = context
+      def initialize(endpoint)
+        @endpoint = endpoint
+        @context = ZMQ::Context.new
       end
     
+      def request(req)
+        socket = @context.socket(ZMQ::REQ)
+        rc = socket.connect(@endpoint)
+        if ZMQ::Util.resultcode_ok?(rc)
+          begin
+            socket.send_string(req)
+            socket.recv_string(res = '')
+            response = nil if res == ''
+          ensure
+            socket.close
+          end
+        else
+          raise RuntimeError, "Couldn't connect to #{ep}"
+        end
+        
+        res
+      end
+ 
+      def register(name, endpoint)
+        request("REGISTER #{name} #{endpoint}")
+      end
+      
       def lookup(name)
-        ENDPOINTS[name][:endpoint]
+        request("LOOKUP #{name}")
       end
     
       def bind(name)
@@ -25,7 +50,7 @@ module Service
         socket = @context.socket(ZMQ::REQ)
         rc = socket.connect(endpoint)
         if ZMQ::Util.resultcode_ok?(rc)
-          proxy = ENDPOINTS[name][:class].new(socket) 
+          proxy = PROXIES[name].new(socket) 
           begin
             yield proxy
           ensure
--- spec/services/platform/service_registry_proxy_spec.rb
+++ spec/services/platform/service_registry_proxy_spec.rb
@@ -2,58 +2,90 @@ require 'spec_helper'
 
 module Service::Platform
   describe ServiceRegistryProxy do
+    before(:each) do
+      @registry = ServiceRegistryProxy.new(nil) 
+    end
+
+    describe "lookup" do
+      it "can be used to look up endpoints" do
+        name = 'ais/message'
+        endpoint = 'tcp://localhost:21002'
+
+        socket = double("Socket")
+        socket.should_receive(:connect) { 0 }
+        socket.should_receive(:send_string).with('LOOKUP ' << name)
+        socket.should_receive(:recv_string) { |s| s.replace(endpoint) }
+        socket.should_receive(:close) { 0 }
+  
+        context = double("Context")
+        context.should_receive(:socket) { socket }
+
+        @registry.context = context
+        @registry.lookup(name).should eq(endpoint)        
+      end
+    end
+
+    describe "register" do
+      it "sends a register request" do
+        name = 'ais/transmitter'
+        endpoint = 'tcp://localhost:22000'
+        
+        socket = double("Socket")
+        socket.should_receive(:connect) { 0 }
+        socket.should_receive(:send_string).with("REGISTER #{name} #{endpoint}") { 0 }
+        socket.should_receive(:recv_string) { '' }
+        socket.should_receive(:close) { 0 }
+  
+        context = double("Context")
+        context.should_receive(:socket) { socket }
+  
+        @registry.context = context
+        @registry.register(name, endpoint)        
+      end
+    end
+        
     describe "bind" do
       it "returns a service binding" do
+        name = 'ais/transmitter'
+        endpoint = 'tcp://localhost:22000'
+        
         socket = double("Socket")
-        socket.stub(:connect) { 0 }
-        socket.stub(:close) { 0 }
+        socket.should_receive(:connect).with(endpoint) { 0 }
+        socket.should_receive(:close) { 0 }
   
         context = double("Context")
-        context.stub(:socket) { socket }
+        context.should_receive(:socket) { socket }
   
-        registry = ServiceRegistryProxy.new context
-        proxies = {'ais/transmitter' => Service::TransmitterProxy, 
-                   'ais/vessels' => Service::VesselServiceProxy}
-        proxies.each do |name, klass|
-          registry.bind(name) do |service|
-            service.should be_a_kind_of(klass) 
-          end
+        @registry.context = context
+        @registry.should_receive(:lookup).with(name) { endpoint }
+        
+        @registry.bind(name) do |service|
+          service.should be_a_kind_of(Service::TransmitterProxy) 
         end      
       end
-  
-      it "can be used to look up endpoints" do
-        registry = ServiceRegistryProxy.new
-        endpoints = {'ais/message' => 'tcp://localhost:21002'}
-        endpoints.each do |name, endpoint|
-          registry.lookup(name).should eq(endpoint)        
-        end
-      end
-  
+
       it "raises an exception when the socket fails" do
         socket = double("Socket")
-        socket.stub(:connect) { -1 }
-  
+        socket.stub(:connect) { -1 }  
         context = double("Context")
         context.stub(:socket) { socket }
-  
-        registry = ServiceRegistryProxy.new(context)
-  
-        socket.stub(:connect) { -1 }
-        registry = ServiceRegistryProxy.new(context)
-        expect { registry.bind 'ais/transmitter' }.to raise_error
+        
+        @registry.context = context  
+        expect { @registry.bind('ais/transmitter') }.to raise_error
       end
   
       it "closes sockets of proxies when stopped" do
         socket = double("Socket")
-        socket.stub(:connect) { 0 }
+        socket.should_receive(:connect) { 0 }
         socket.should_receive(:close)
   
         context = double("Context")
         context.stub(:socket) { socket }
   
-        registry = ServiceRegistryProxy.new context
-        registry.bind('ais/transmitter') {|service| }
+        @registry.context = context
+        @registry.stub(:lookup) { nil }
+        @registry.bind('ais/transmitter') {|service| nil }
       end
-    end
+    end    
   end
 end
