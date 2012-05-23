a8856f6478a09d915a2f550720af93f7375bf8b6
John <j.a.hornlopes@student.tudelft.nl>
2012-05-23 15:13:44 +0200

    Added a spec for service manager

--- spec/services/platform/service_manager_spec.rb
+++ spec/services/platform/service_manager_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 module Service::Platform
   describe ServiceManager do
+    before(:each) do
+      @manager = ServiceManager.new
+    end
+    
     it "starts and stops" do
-      sm = ServiceManager.new
-      sm.start
-      sm.stop
+      @manager.start
+      @manager.stop
+    end
+    
+    it "chooses a random endpoint for the registry" do
+      @manager.get_registry_endpoint.should match(/tcp:\/\/localhost:21\d{3}/)
     end
   end
 end
