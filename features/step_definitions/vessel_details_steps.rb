642120a450d4d7febf4d8ce4eb4a525515c79648
John <j.a.hornlopes@student.tudelft.nl>
2012-05-25 18:35:45 +0200

    Updated scenarios for next story, updated acceptance tests with step definitions for this story

---
+++ features/step_definitions/vessel_details_steps.rb
@@ -0,0 +1,39 @@
+Given /^vessel "(.*?)" with details:$/ do |name, table|
+  @fields = table.rows_hash 
+  
+  if fields['Class'] == 'A'
+    vessel_class = Domain::Vessel::CLASS_A
+  else
+    vessel_class = Domain::Vessel::CLASS_B
+  end
+    
+  @vessel = Domain::Vessel.new(fields['MMSI'], vessel_class)
+  @vessel.name =  name
+  @vessel.type = Domain::VesselType.from_str(fields['Type'])
+  @vessel.heading = fields['Heading'].to_f
+  @vessel.speed = fields['Speed'].to_f
+  @vessel.position = Domain::LatLon.from_str(fields['Position'])
+end
+
+
+When /^"(.*?)" sends a position report$/ do |arg1|
+  @registry.bind('ais/transmitter') do |service|
+    service.send_position_report_for @vessel
+  end
+end
+
+When /^"(.*?)" sends a voyage report$/ do |arg1|
+  @registry.bind('ais/transmitter') do |service|
+    service.send_static_report_for @vessel
+  end
+end
+
+When /^I select vessel "(.*?)" on the map$/ do |name|
+  page.evaluate_script('map.clickMarker(new LatLon(' << point.lat.to_s << ',' << point.lon.to_s << '))')
+end
+
+Then /^I should see all details of vessel "(.*?)"$/ do |name|
+  @fields.each do |_, value|
+    page.has_content?(value).should be_true
+  end
+end
--- features/vessel_details.feature
+++ features/vessel_details.feature
@@ -3,14 +3,16 @@ Feature: Vessel Details
   As a coast guard
   I want to be able to select a vessel and see its details
   
+  @wip
   Scenario:
     Given vessel "Sea Lion" with details:
-       | detail   | value         |
-       | Name     | Sea Lion      |
        | MMSI     | 245000000     |
+       | Class    | A             |
        | Type     | Cargo ship    |
        | Position | 51.99N, 4.05E |
        | Heading  | 290.1         |
        | Speed    | 13.1          |
-     When I select vessel "Sea Lion" on the map
-     Then I should see the details of vessel "Sea Lion"
+     When "Sea Lion" sends a position report
+      And "Sea Lion" sends a voyage report 
+      And I select vessel "Sea Lion" on the map
+     Then I should see all details of vessel "Sea Lion"
