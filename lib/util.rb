9e6a693607a5bf57653df0cb9afb8cf57f49ba81
John <j.a.hornlopes@student.tudelft.nl>
2012-05-29 10:51:06 +0200

    Fixed check to see if provided loglevel is a valid constant, previous check crashed when a lower case value was provided, since all constants must be upper case

--- lib/util.rb
+++ lib/util.rb
@@ -2,10 +2,16 @@ require 'logger'
 
 module Util
   def get_log(progname)
+    valid_levels = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL']
     logfile = File.dirname(__FILE__) << "/../log/log-#{progname}.log"
     log = Logger.new(logfile)
-    if ENV.has_key?('LOGLEVEL') and Logger::Severity.const_defined?(ENV['LOGLEVEL']) 
-      log.level = Logger::Severity.const_get(ENV['LOGLEVEL'])
+    if ENV.has_key?('LOGLEVEL')
+      if valid_levels.include?(ENV['LOGLEVEL'])
+        log.level = Logger::Severity.const_get(ENV['LOGLEVEL'])  
+      else
+        log.level = Logger::ERROR
+        log.error("Invalid value for LOGLEVEL: #{ENV['LOGLEVEL']}")
+      end
     else
       log.level = Logger::ERROR
     end
