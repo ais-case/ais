101b788223625a9dbb6c1bd2a823ec56289f798d
John <j.a.hornlopes@student.tudelft.nl>
2012-05-29 08:04:46 +0200

    Clean up spec by moving test data generation to before clause

--- spec/services/vessel_proxy_spec.rb
+++ spec/services/vessel_proxy_spec.rb
@@ -2,49 +2,40 @@ require 'spec_helper'
 
 module Service
   describe VesselProxy do
-    it "requests vessel information from the Vessel service" do
-      
-      # Some test data
-      vessel1 = Domain::Vessel.new(1234, Domain::Vessel::CLASS_A)
-      vessel1.position = Domain::LatLon.new(3.0, 4.0) 
-      vessel2 = Domain::Vessel.new(5678, Domain::Vessel::CLASS_A)
-      vessel2.position = Domain::LatLon.new(5.0, 6.0)
-      vessels = [vessel1, vessel2] 
-
+    before(:each) do
+        vessel1 = Domain::Vessel.new(1234, Domain::Vessel::CLASS_A)
+        vessel1.position = Domain::LatLon.new(3.0, 4.0) 
+        vessel2 = Domain::Vessel.new(5678, Domain::Vessel::CLASS_A)
+        vessel2.position = Domain::LatLon.new(5.0, 6.0)
+        @vessels = [vessel1, vessel2] 
+    end
+  
+    it "requests vessel information from the Vessel service" do      
       # Mock socket should be used correctly      
       socket = double('Socket')
       socket.should_receive(:send_string).with('')
       socket.should_receive(:recv_string) do |str|
-        str.replace(Marshal.dump(vessels))
+        str.replace(Marshal.dump(@vessels))
       end
       
       # Mock should return correct list of vessels
       t = VesselProxy.new(socket)
-      t.vessels.should eq(vessels)
+      t.vessels.should eq(@vessels)
     end  
 
     it "requests only vessels within the required area" do
-      
-      # Some test data
-      vessel1 = Domain::Vessel.new(1234, Domain::Vessel::CLASS_A)
-      vessel1.position = Domain::LatLon.new(3.0, 4.0) 
-      vessel2 = Domain::Vessel.new(5678, Domain::Vessel::CLASS_A)
-      vessel2.position = Domain::LatLon.new(5.0, 6.0)
-      vessels = [vessel1, vessel2] 
-
-      latlon1 = Domain::LatLon.new(3.0, 4.0)
-      latlon2 = Domain::LatLon.new(3.0, 4.0)
+      latlon1 = Domain::LatLon.new(3.0, 3.0)
+      latlon2 = Domain::LatLon.new(5.0, 6.0)
 
       # Mock socket should be used correctly      
       socket = double('Socket')
       socket.should_receive(:send_string).with(Marshal.dump([latlon1, latlon2]))
       socket.should_receive(:recv_string) do |str|
-        str.replace(Marshal.dump(vessels))
+        str.replace(Marshal.dump(@vessels))
       end
       
-      # Mock should return correct list of vessels
       t = VesselProxy.new(socket)
-      t.vessels(latlon1, latlon2).should eq(vessels)
+      t.vessels(latlon1, latlon2).should eq(@vessels)
     end  
   end
 end
