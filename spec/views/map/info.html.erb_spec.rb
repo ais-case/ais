4e490c6dc4a6de2b9c14ad4c9742a604164432d1
John <j.a.hornlopes@student.tudelft.nl>
2012-05-29 18:53:19 +0200

    Added view for info

---
+++ spec/views/map/info.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "map/info.html.erb" do
+  it "returns info for vessel with given mssi" do
+    vessel = Domain::Vessel.new(898989, Domain::Vessel::CLASS_A)
+    vessel.position = Domain::LatLon.new(52.1, 3.92)
+    vessel.heading = 180
+    vessel.speed = 30.56
+    vessel.type = Domain::VesselType.from_str("Passenger")
+    
+    assign(:vessel, vessel)
+    render
+
+    details = [vessel.mmsi, vessel.position, vessel.heading, vessel.speed, vessel.type.description]
+    texts = details.map { |o| o.to_s }
+    texts.each do |text|
+      if not rendered.include?(text)
+        err = "Text '#{text}' not found in view"
+        raise err
+      end 
+    end
+  end
+end
