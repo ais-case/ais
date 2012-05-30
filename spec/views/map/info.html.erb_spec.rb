22f8c49d873e7ac9fff08f43886cca9cda3c1c0f
John <j.a.hornlopes@student.tudelft.nl>
2012-05-30 18:22:12 +0200

    Fix position info not shown for vessels of unknown type

--- spec/views/map/info.html.erb_spec.rb
+++ spec/views/map/info.html.erb_spec.rb
@@ -27,4 +27,23 @@ describe "map/info.html.erb" do
     assign(:vessel, vessel)
     render
   end
+  
+  it "handles vessel with only position info" do
+    vessel = Domain::Vessel.new(898989, Domain::Vessel::CLASS_A)
+    vessel.position = Domain::LatLon.new(52.1, 3.92)
+    vessel.heading = 180
+    vessel.speed = 30.56
+    
+    assign(:vessel, vessel)
+    render
+
+    details = [vessel.mmsi, vessel.position, vessel.heading, vessel.speed]
+    texts = details.map { |o| o.to_s }
+    texts.each do |text|
+      if not rendered.include?(text)
+        err = "Text '#{text}' not found in view"
+        raise err
+      end 
+    end
+  end
 end
