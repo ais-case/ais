02fd7391ae0b2904556dc37a5562ad07085c461d
John <j.a.hornlopes@student.tudelft.nl>
2012-05-30 14:33:07 +0200

    Handle payloads of class B messages in MessageFactory

--- lib/domain/ais/message_factory.rb
+++ lib/domain/ais/message_factory.rb
@@ -4,6 +4,7 @@ require_relative 'message1'
 require_relative 'message5'
 require_relative 'message18'
 require_relative 'message19'
+require_relative 'message24'
 require_relative '../vessel'
 require_relative '../vessel_type'
 
@@ -19,6 +20,7 @@ module Domain
         
         decoded = SixBitEncoding.decode(payload)
         msg_type = decoded[0..5].to_i(2)
+        message = nil
         if msg_type == 1 or msg_type == 2 or msg_type == 3
           if decoded.length >= 168
             message = Message1.new(Datatypes::UInt.from_bit_string(decoded[8..37]).value)
@@ -32,22 +34,50 @@ module Domain
             
             heading = Datatypes::UInt.from_bit_string(decoded[128..136]).value
             message.heading = (heading == 511) ? nil : heading
-            
-            message
-          else
-            nil
           end
         elsif msg_type == 5
           if decoded.length >= 424
             message = Message5.new(Datatypes::UInt.from_bit_string(decoded[8..37]).value)
             message.vessel_type = Domain::VesselType::new(Datatypes::UInt.from_bit_string(decoded[232..239]).value)
-            message
-          else
-            nil
+          end
+        elsif msg_type == 18
+          if decoded.length >= 168
+            message = Message18.new(Datatypes::UInt.from_bit_string(decoded[8..37]).value)
+            lon = Datatypes::Int.from_bit_string(decoded[57..84])
+            lat = Datatypes::Int.from_bit_string(decoded[85..111])
+            message.lon = lon.value / 600000.0
+            message.lat = lat.value / 600000.0
+  
+            speed = Datatypes::UInt.from_bit_string(decoded[46..55]).value
+            message.speed = (speed == 1023) ? nil : speed.to_f / 10.0
+            
+            heading = Datatypes::UInt.from_bit_string(decoded[124..132]).value
+            message.heading = (heading == 511) ? nil : heading
+          end
+        elsif msg_type == 19
+          if decoded.length >= 312
+            message = Message19.new(Datatypes::UInt.from_bit_string(decoded[8..37]).value)
+            lon = Datatypes::Int.from_bit_string(decoded[57..84])
+            lat = Datatypes::Int.from_bit_string(decoded[85..111])
+            message.lon = lon.value / 600000.0
+            message.lat = lat.value / 600000.0
+  
+            speed = Datatypes::UInt.from_bit_string(decoded[46..55]).value
+            message.speed = (speed == 1023) ? nil : speed.to_f / 10.0
+            
+            heading = Datatypes::UInt.from_bit_string(decoded[124..132]).value
+            message.heading = (heading == 511) ? nil : heading
+          end
+        elsif msg_type == 24
+          if decoded.length >= 168 and decoded[38..39] == '01'
+            message = Message24.new(Datatypes::UInt.from_bit_string(decoded[8..37]).value)
+            message.vessel_type = Domain::VesselType::new(Datatypes::UInt.from_bit_string(decoded[40..47]).value)
           end
         else
           raise "Unknown message type #{msg_type}"
         end
+        
+        message
       end
       
       def create_position_report(vessel)
@@ -64,7 +94,11 @@ module Domain
       end
 
       def create_static_info(vessel)
-        message = Message5.new(vessel.mmsi)
+        if vessel.vessel_class == Domain::Vessel::CLASS_B
+          message = Message24.new(vessel.mmsi)
+        else
+          message = Message5.new(vessel.mmsi)
+        end
         message.vessel_type = vessel.type
         message
       end
--- spec/domain/ais/message_factory_spec.rb
+++ spec/domain/ais/message_factory_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Domain::AIS
   describe MessageFactory do
     describe "fromPayload" do
-      it "can create a position report message from a payload" do
+      it "can create a class A position report message from a payload" do
         payload = "13`wgT0P5fPGmDfN>o?TN2NN2<05"
         mf = MessageFactory.new
         msg = MessageFactory.fromPayload(payload)
@@ -14,14 +14,45 @@ module Domain::AIS
         msg.speed.should be_within(0.1).of(36.6)
         msg.heading.should be(79)
       end
+
+      it "can create a class B position report message from a payload" do
+        payload = "B6:fOUh0=R1oRQSC=jo9D7b61P06"
+        mf = MessageFactory.new
+        msg = MessageFactory.fromPayload(payload)
+        msg.mmsi.should eq(413900695)
+        msg.vessel_class.should eq(Domain::Vessel::CLASS_B)
+        msg.lat.should be_within(1.0/1_000_000).of(23.070368)
+        msg.lon.should be_within(1.0/1_000_000).of(113.480218)
+        msg.speed.should be_within(0.1).of(5.4)
+        msg.heading.should be(15)
+
+        payload = "C6:fOUh0=R1oRQSC=jo9D7b61P06000000000000000000000000"
+        mf = MessageFactory.new
+        msg = MessageFactory.fromPayload(payload)
+        msg.mmsi.should eq(413900695)
+        msg.vessel_class.should eq(Domain::Vessel::CLASS_B)
+        msg.lat.should be_within(1.0/1_000_000).of(23.070368)
+        msg.lon.should be_within(1.0/1_000_000).of(113.480218)
+        msg.speed.should be_within(0.1).of(5.4)
+        msg.heading.should be(15)
+      end
   
-      it "can create a static info message from a payload" do
+      it "can create a class A static info message from a payload" do
         payload = "53u=:PP00001<H?G7OI0ThuB37G61<F22222220j1042240Ht2P00000000000000000008"
         mf = MessageFactory.new
         msg = MessageFactory.fromPayload(payload)
         msg.mmsi.should eq(265505410)
         msg.vessel_class.should eq(Domain::Vessel::CLASS_A)
         msg.vessel_type.should eq(Domain::VesselType.new(50))
+      end
+
+      it "can create a class B static info message from a payload" do
+        payload = "H44?BB4lDB1>C1CEC130001@F270"
+        mf = MessageFactory.new
+        msg = MessageFactory.fromPayload(payload)
+        msg.mmsi.should eq(272880200)
+        msg.vessel_class.should eq(Domain::Vessel::CLASS_B)
+        msg.vessel_type.should eq(Domain::VesselType.new(52))
       end
       
       it "returns null for messages of incorrect length" do
@@ -65,18 +96,33 @@ module Domain::AIS
         msg.heading.should eq(90)
       end
     end
-    it "can create static info messages from vessel info" do
-      vessel_type = Domain::VesselType.from_str("Tanker")
-      vessel = Domain::Vessel.new(1234, Domain::Vessel::CLASS_A)
-      vessel.position = Domain::LatLon.new(3.0, 4.0)
-      vessel.type = vessel_type  
-
-      mf = MessageFactory.new
-      msg = mf.create_static_info(vessel)
-      msg.type.should eq(5)
-      msg.mmsi.should eq(1234)
-      msg.vessel_type.should eq(vessel_type)
+    
+    describe "create_static_info" do
+      it "creates static info messages from class A vessel objects" do
+        vessel_type = Domain::VesselType.from_str("Tanker")
+        vessel = Domain::Vessel.new(1234, Domain::Vessel::CLASS_A)
+        vessel.position = Domain::LatLon.new(3.0, 4.0)
+        vessel.type = vessel_type  
+  
+        mf = MessageFactory.new
+        msg = mf.create_static_info(vessel)
+        msg.type.should eq(5)
+        msg.mmsi.should eq(1234)
+        msg.vessel_type.should eq(vessel_type)
+      end
+      
+      it "creates static info messages from class B vessel objects" do
+        vessel_type = Domain::VesselType.from_str("Tanker")
+        vessel = Domain::Vessel.new(1234, Domain::Vessel::CLASS_B)
+        vessel.position = Domain::LatLon.new(3.0, 4.0)
+        vessel.type = vessel_type  
+  
+        mf = MessageFactory.new
+        msg = mf.create_static_info(vessel)
+        msg.type.should eq(24)
+        msg.mmsi.should eq(1234)
+        msg.vessel_type.should eq(vessel_type)
+      end
     end
-
   end
 end
