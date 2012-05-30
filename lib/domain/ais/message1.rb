0f59de5415d9d330def01d3eb5f7eb42be4482dd
John <j.a.hornlopes@student.tudelft.nl>
2012-05-30 11:08:22 +0200

    Enable encoding of speed and heading

--- lib/domain/ais/message1.rb
+++ lib/domain/ais/message1.rb
@@ -4,7 +4,7 @@ module Domain
   module AIS
     class Message1
       attr_reader :mmsi, :vessel_class, :type
-      attr_accessor :lat, :lon
+      attr_accessor :lat, :lon, :speed, :heading
       
       def initialize(mmsi)
         @mmsi = mmsi
@@ -25,8 +25,21 @@ module Domain
         # mmsi
         payload << int_class.new(@mmsi).bit_string(30)
         
-        # nav status, rot, sog, accuracy
-        payload << '0' * 23
+        # nav status, rot
+        payload << '0' * 12
+        
+        # speed
+        if not @speed
+          speed = 1023
+        elsif @speed > 102.2
+          speed = 1022
+        else
+          speed = (@speed * 10).to_i
+        end
+        payload << int_class.new(speed).bit_string(10)
+        
+        # accuracy
+        payload << '0'
         
         # long
         payload << int_class.new(@lon * 600_000).bit_string(28)
@@ -34,8 +47,15 @@ module Domain
         # lat
         payload << int_class.new(@lat * 600_000).bit_string(27)
         
+        # course
+        payload << '0' * 12 
+        
+        # heading
+        heading = @heading ? @heading : 511
+        payload << int_class.new(heading).bit_string(9)
+
         # rest of message
-        payload << '0' * 53
+        payload << '0' * 32
         
         payload
       end
--- spec/domain/ais/message1_spec.rb
+++ spec/domain/ais/message1_spec.rb
@@ -9,12 +9,16 @@ module Domain::AIS
       m.vessel_class.should eq(Domain::Vessel::CLASS_A)
     end
     
-    it "has lat and lon properties" do  
+    it "has lat, lon, speed and heading properties" do  
       m = Message1.new(244314000)
       m.lat = 1.0
       m.lat.should eq(1.0)
       m.lon = -1.0
       m.lon.should eq(-1.0)
+      m.speed = 53.6
+      m.speed.should be_within(0.01).of(53.6)
+      m.heading = 35
+      m.heading.should eq(35)
     end
     
     describe "payload" do
@@ -22,7 +26,9 @@ module Domain::AIS
         m = Message1.new(1234)
         m.lat = 3.0
         m.lon = 4.0
-        Domain::AIS::SixBitEncoding.encode(m.payload).should eq("10004lP0000BCp01eo@0000000000")
+        m.speed = 54.1
+        m.heading = 253
+        Domain::AIS::SixBitEncoding.encode(m.payload).should eq("10004lP08M0BCp01eo@007r000000")
       end
     end
   end
