69f7fb574e121bcb2768be20200fd1e67e0d2c65
John <j.a.hornlopes@student.tudelft.nl>
2012-06-01 12:37:49 +0200

    Extended marker class to pick the correct icon based on vessel heading

--- lib/marker.rb
+++ lib/marker.rb
@@ -1,17 +1,41 @@
 class Marker
-  attr_reader :id, :position
+  attr_reader :id, :position, :icon
   
-  def initialize(id, position)
+  def initialize(id, position, icon)
     @id = id
     @position = position
+    @icon = icon
+  end
+  
+  def self.icon_from_vessel(vessel)
+    dir = case vessel.heading
+    when (0..45)
+      'n'
+    when (46..134)
+      'e'
+    when (135..225)
+      's'
+    when (226..314)
+      'w'
+    when (315..360)
+    else 
+      nil
+    end
+    
+    if dir
+      "/markers/v_#{dir}.png"
+    else
+      "/markers/v.png"
+    end
   end
   
   def self.from_vessel(vessel)
-    Marker.new(vessel.mmsi, vessel.position)
+    Marker.new(vessel.mmsi, vessel.position, self.icon_from_vessel(vessel))
   end
   
   def ==(other)
     (@id == other.id) and 
-    (@position == other.position)
+    (@position == other.position) and
+    (@icon == other.icon)
   end
 end
--- spec/lib/marker_spec.rb
+++ spec/lib/marker_spec.rb
@@ -1,30 +1,50 @@
 require 'spec_helper'
 
 describe Marker do
-  it "has an id and position" do
+  it "has an id, position and icon" do
     id = 4242
     position = Domain::LatLon.new(10, 20) 
-    marker = Marker.new(id, position)
+    marker = Marker.new(id, position, '/ol/img/marker.png')
     marker.id.should eq(id)
     marker.position.should eq(position)
+    marker.icon.should eq('/ol/img/marker.png');
   end
 
-  it "can be created from a Domain::Vessel" do
-    vessel = Domain::Vessel.new(4321, Domain::Vessel::CLASS_A)
-    vessel.position = Domain::LatLon.new(30, 40)
-    marker = Marker.from_vessel(vessel)
-    marker.id.should eq(vessel.mmsi)
-    marker.position.should eq(vessel.position)
-  end
-  
   it "can be compared to other markers" do
-    m1 = Marker.new(1, Domain::LatLon.new(10, 20))
-    m2 = Marker.new(1, Domain::LatLon.new(10, 20))
-    m3 = Marker.new(2, Domain::LatLon.new(10, 20))
-    m4 = Marker.new(2, Domain::LatLon.new(20, 20))
+    m1 = Marker.new(1, Domain::LatLon.new(10, 20), '/ol/img/marker.png')
+    m2 = Marker.new(1, Domain::LatLon.new(10, 20), '/ol/img/marker.png')
+    m3 = Marker.new(2, Domain::LatLon.new(10, 20), '/ol/img/marker.png')
+    m4 = Marker.new(2, Domain::LatLon.new(20, 20), '/ol/img/marker.png')
+    m5 = Marker.new(2, Domain::LatLon.new(20, 20), '/ol/img/marker2.png')
     
     m1.should eq(m2)
     m2.should_not eq(m3)
     m3.should_not eq(m4)
+    m4.should_not eq(m5)
+  end
+
+  describe "from_vessel" do
+    it "can create a marker from a Domain::Vessel" do
+      vessel = Domain::Vessel.new(4321, Domain::Vessel::CLASS_A)
+      vessel.position = Domain::LatLon.new(30, 40)
+      marker = Marker.from_vessel(vessel)
+      marker.id.should eq(vessel.mmsi)
+      marker.position.should eq(vessel.position)
+      marker.icon.should eq('/markers/v.png')
+    end
+  end
+  
+  describe "icon_from_vessel" do
+    it "chooses the correct icon based on heading" do
+      vessel = Domain::Vessel.new(4321, Domain::Vessel::CLASS_A)
+      vessel.position = Domain::LatLon.new(30, 40)
+      marker = Marker.from_vessel(vessel)
+      
+      {10 => 'n', 80 => 'e', 138 => 's'}.each do |heading,icon|    
+        vessel.heading = heading
+        marker = Marker.from_vessel(vessel)
+        marker.icon.should eq("/markers/v_#{icon}.png")
+      end
+    end
   end
 end
