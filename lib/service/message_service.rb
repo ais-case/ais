073abdf371237cd589a2059cc12813415086d1b5
John <j.a.hornlopes@student.tudelft.nl>
2012-06-08 18:46:11 +0200

    Refactor message service to use combiner service as source of messages

--- lib/service/message_service.rb
+++ lib/service/message_service.rb
@@ -4,6 +4,7 @@ require_relative '../util'
 require_relative '../domain/ais/checksums'
 require_relative '../domain/ais/six_bit_encoding'
 require_relative 'platform/base_service'
+require_relative 'platform/subscriber_service'
 
 module Service
   class MessageService < Platform::BaseService
@@ -13,6 +14,9 @@ module Service
       @registry = registry
       @log = Util::get_log('message')
       @payload = ''
+      
+      filter = ['PAYLOAD ']
+      @combiner = Platform::SubscriberService.new(method(:process_message), filter, @log)
     end
     
     def start(endpoint)
@@ -21,56 +25,32 @@ module Service
       rc = @pub_socket.bind(endpoint)
       raise "Couldn't bind to socket" unless ZMQ::Util.resultcode_ok?(rc)
       
-      @subscriber_thread = Thread.new(TCPSocket.new('localhost', 20000)) do |socket|
-        begin
-          loop do
-            data = socket.gets
-            break if data == nil
-            @log.debug("Received raw message: #{data}")
-            process_message(data)
-          end
-       rescue
-          puts $!
-          raise
-        ensure
-          socket.close
-        end
-      end      
-      
+      combiner_endpoint = @registry.lookup('ais/combiner')
+      @combiner.start(combiner_endpoint) if combiner_endpoint
+
       register_self('ais/message', endpoint)
       @log.info("Service started")
     end
     
     def wait
-      @subscriber_thread.join
+      @combiner.wait
     end
     
     def stop
-      @pub_socket.close
-      
-      @subscriber_thread.kill if @subscriber_thread
-      @subscriber_thread = nil
+      @combiner.stop
+      @pub_socket.close      
     end
     
     def process_message(data)
-      valid_message = false
-      @registry.bind('ais/checksum') do |service|
-        valid_message = service.verify(data)
-      end      
-      return unless valid_message
+      # Remove header
+      payload = data.split(' ')[1] 
       
-      preamble, fragment_count, fragment_number, id, channel, payload, suffix = data.split(',')
-      
-      @payload << payload
-      
-      if fragment_count == fragment_number
-        type = nil
-        @registry.bind('ais/payload-decoder') do |service|
-          type = service.decode(@payload[0]).to_i(2)
-        end
-        publish_message(type, @payload.dup)
-        @payload = ''
+      # Determine message type by examining first byte of payload
+      type = nil
+      @registry.bind('ais/payload-decoder') do |service|
+        type = service.decode(payload[0]).to_i(2)
       end
+      publish_message(type, payload)
     end
     
     def publish_message(type, payload)
--- spec/lib/services/message_service_spec.rb
+++ spec/lib/services/message_service_spec.rb
@@ -3,89 +3,59 @@ require 'spec_helper'
 module Service
   describe MessageService do
     before(:all) do
-      @sample_message = "!AIVDM,1,1,,B,13OF<80vh2wgiJJNes7EMGrD0<0e,0*00"
-      @sample_type    =                1
-      @sample_payload =               "13OF<80vh2wgiJJNes7EMGrD0<0e"
+      @sample_type    = 1
+      @sample_payload = "13OF<80vh2wgiJJNes7EMGrD0<0e"
     end
     
     before(:each) do
       @registry = MockRegistry.new
-      sleep(0.1)
-      @server_queue = Queue.new
-      @server = Thread.new(TCPServer.new(20000)) do |socket|
-        begin
-          client = socket.accept
-          client.puts(@server_queue.pop)
-        ensure 
-          socket.close
-        end
-      end
+      Thread.new do end
     end
       
-    after(:each) do
-      @server.kill
-      @server = nil
-      @server_queue = nil
-    end
-    
     it_behaves_like "a service"
     
-    it "listens for raw AIS data from a local TCP server on port 20000" do
-      service = MessageService.new(@registry)
-      service.should_receive(:process_message).with(@sample_message << "\n")
-      service.start('tcp://*:28000')
-      
-      # Wait for mock TCP server to finish request
-      timeout(1) do
-        @server_queue.push(@sample_message)
-        @server.join
-      end
-
-      service.stop
-    end
-  
     it "publishes processed messages" do
       # Set up proxy for decoder and checksum
       proxy = double('Proxy')
       proxy.stub(:decode).and_return('1')
-      proxy.stub(:verify).and_return(true)
       @registry.stub(:bind).and_yield(proxy)
 
       service = MessageService.new(@registry)
       service.should_receive(:publish_message).with(@sample_type, @sample_payload)
-      service.process_message(@sample_message)
+      service.process_message('PAYLOAD ' << @sample_payload)
     end
 
-    it "does not publish messages with invalid checksums" do
-      # Set up proxy for decoder and checksum
-      proxy = double('Proxy')
-      proxy.stub(:decode).and_return('101')
-      proxy.stub(:verify).and_return(false)
-      @registry.stub(:bind).and_yield(proxy)
+    it "listens for AIS payloads" do
+      payloads = ['23afKn5P070CqEdMd<TqIwv6081W']
+      
+      ctx = ZMQ::Context.new
+      sock = ctx.socket(ZMQ::PUB)
+      begin
+        rc = sock.bind('tcp://*:21013')
+        ZMQ::Util.resultcode_ok?(rc).should be_true
+        @registry.register('ais/combiner', 'tcp://localhost:21013')
+        
+        service = (Class.new(MessageService) do
+          attr_reader :received_data
+          def process_message(data)
+            @received_data = data
+          end
+        end).new(@registry)
 
-      message = @sample_message.dup
-      message[20] = 'E'
-      service = MessageService.new(@registry)
-      service.should_not_receive(:publish_message)
-      service.process_message(message)
+        service.start('tcp://*:23001')
+        payloads.each do |payload|
+          sock.send_string('PAYLOAD ' << payload)
+
+          # Give service time to receive and process message
+          sleep(0.1)
+          service.received_data.should eq('PAYLOAD ' << payload)  
+        end
+        service.stop
+      ensure
+        sock.close
+      end
     end
 
-    it "aggregates multi-fragment messages before publishing" do
-      payload1 = "53aaW@00000000000000000000000000000000160000000000000000"
-      payload2 = "00000000000000"
-
-      # Set up proxy for decoder and checksum
-      proxy = double('Proxy')
-      proxy.stub(:decode).and_return('101')
-      proxy.stub(:verify).and_return(true)
-      @registry.stub(:bind).and_yield(proxy)
-
-      service = MessageService.new(@registry)
-      service.should_receive(:publish_message).with(5, payload1 + payload2)
-      service.process_message("!AIVDM,2,1,,A,#{payload1},0*33")
-      service.process_message("!AIVDM,2,2,,A,#{payload2},0*26")
-    end
-    
     it "broadcasts published messages to subscribers" do
       handler = double('Subscriber')
       handler.should_receive(:handle_request).with("#{@sample_type} #{@sample_payload}")
