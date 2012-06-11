97d49d790efb316b147e9664e264d20780850ce8
John <j.a.hornlopes@student.tudelft.nl>
2012-06-12 01:06:42 +0200

    Save log files if scenario fails, delete them otherwise

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -5,8 +5,22 @@ Before do
   @manager = Service::Platform::ServiceManager.new
   @manager.start
   @registry = @manager.registry_proxy
+  @failed_scenarios = 0
 end
 
 After do |scenario|
   @manager.stop
+  
+  if scenario.failed?
+    @failed_scenarios += 1
+    #name = scenario.to_sexp[3]
+    name = @failed_scenarios.to_s
+    logbase = Rails.root.join('log', name.gsub(' ', '_'))
+    FileUtils.makedirs(logbase) 
+    Dir.glob(Rails.root.join('log', '*.log')).each do |log|
+      FileUtils.copy(log, File.join(logbase, File.basename(log)))
+    end
+  end
+  
+  FileUtils.rm(Dir.glob(Rails.root.join('log', '*.log')))
 end
